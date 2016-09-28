e3d4626581d165e7179360e8c4d5a8570362ecd5
tucici <you@yourdomain.com>
2016-09-28 16:16:34 +0800

    Initial Commit

---
+++ CustomImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomImage
+//
+//  Created by mac1 on 16/9/28.
+//  Copyright © 2016年 Tucici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomImageTests/CustomImageTests.m
@@ -0,0 +1,39 @@
+//
+//  CustomImageTests.m
+//  CustomImageTests
+//
+//  Created by mac1 on 16/9/28.
+//  Copyright © 2016年 Tucici. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomImageTests : XCTestCase
+
+@end
+
+@implementation CustomImageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
